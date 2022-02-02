5341ae25e569cad160570b4ae0c3655da10092b8
francois <francois.michaudon@53js.fr>
2022-02-02 12:53:38 +0100

    fix: url API itunes

--- src/lib/fetchItunesSongs.js
+++ src/lib/fetchItunesSongs.js
@@ -2,8 +2,13 @@ const API = 'https://itunes.apple.com/search';
 
 export const fetchItunesSongs = async (term) => {
 	try {
-		const url = `${API}?term=${term}`;
-		const response = await fetch(url);
+		const url = `${API}?term=${term}&enitity=music`;
+		const response = await fetch(url, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			mode: 'cors',
+		});
 		const responseJson = await response.json();
 		return responseJson;
 	} catch (err) {
